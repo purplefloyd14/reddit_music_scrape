a642896306864c75dbecd13cff775b6cd5b4fdc5
Bradneal14 <bradleyhneal@gmail.com>
2016-04-09 19:10:50 -0700

    watir and phantomjs both working. Making good progress toward goal

--- open_reddit.rb
+++ open_reddit.rb
@@ -3,16 +3,18 @@ require "open-uri"
 require 'json'
 require "tempfile"
 require 'watir-webdriver'
-require 'headless'
+require "phantomjs"
 
-
-
-headless = Headless.new
-headless.start
-b = Watir::Browser.start 'www.google.com'
+b = Watir::Browser.new :firefox
+b.goto 'www.anything2mp3.com'
+f = b.form(:id, "videoconverter-convert-form")
+b.text_field(:id, 'edit-url').set('https://www.youtube.com/watch?v=bQDcSkGWvfc&nohtml5=False')
+f.submit
+b.button(:class => 'success').wait_until_present
 puts b.title
 b.close
-headless.destroy
+
+## https://watirwebdriver.com/waiting/
 
 data_from_reddit = open("http://www.reddit.com/r/listentothis/top.json?sort=top&t=month&limit=10")
 
@@ -22,7 +24,7 @@ def find_title(thing)
   #then it adds all of the titles from the data to an array and returns that array
   all_titles = []
   thing["data"]["children"].each do |post|
-    all_titles << post["data"]["title"]
+    all_titles << post["data"]["url"]
   end
   p all_titles
 end
@@ -51,6 +53,6 @@ end
 
 
 
-
-
+#
+#
 # execute_everything(data_from_reddit)
