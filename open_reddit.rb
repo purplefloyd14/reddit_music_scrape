ee991cd24a7af2f140a78a3616bd409346e6f3e3
Bradneal14 <bradleyhneal@gmail.com>
2016-04-10 16:13:56 -0700

    switches strategies due to problems downloading from headless browser. Now trying with youtube-dl

--- open_reddit.rb
+++ open_reddit.rb
@@ -4,20 +4,25 @@ require 'json'
 require "tempfile"
 require 'watir-webdriver'
 require "phantomjs"
+require "date"
 
-b = Watir::Browser.new :firefox
-b.goto 'www.anything2mp3.com'
-f = b.form(:id, "videoconverter-convert-form")
-b.text_field(:id, 'edit-url').set('https://www.youtube.com/watch?v=bQDcSkGWvfc&nohtml5=False')
-f.submit
-b.button(:class => 'success').wait_until_present
-puts b.title
-b.close
+# b = Watir::Browser.new :firefox
+# b.goto 'www.anything2mp3.com'
+# f = b.form(:id, "videoconverter-convert-form")
+# b.text_field(:id, 'edit-url').set('https://www.youtube.com/watch?v=bQDcSkGWvfc&nohtml5=False')
+# f.submit
+# b.button(:class => 'success').wait_until_present
+# b.button(:class => 'success').click
+# puts b.title
+# b.close
 
+# exec "youtube-dl -x -ohttps://www.youtube.com/watch?v=MUINFs1Sp94"
 ## https://watirwebdriver.com/waiting/
 
 data_from_reddit = open("http://www.reddit.com/r/listentothis/top.json?sort=top&t=month&limit=10")
 
+month = Date::MONTHNAMES[Date.today.month]
+p  month
 
 def find_title(thing)
   #this method iterates through the data for each post it is given
@@ -47,10 +52,16 @@ end
 
 def execute_everything(data)
   # this method just calls the other two methods in order
+  # returns array so song urls 
   objectified_data = make_string(data)
   find_title(objectified_data)
 end
 
+def run_download(url)
+end
+
+def make_dir(month)
+end
 
 
 #
