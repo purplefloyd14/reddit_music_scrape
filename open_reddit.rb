3fc94f92cbc4f0c293b47e2312d260eeb603a768
Bradneal14 <bradleyhneal@gmail.com>
2016-04-12 13:51:48 -0700

    adds methods for metadata tagging

--- open_reddit.rb
+++ open_reddit.rb
@@ -2,12 +2,16 @@ require "open-uri"
 require 'json'
 require "tempfile"
 require "date"
+require "mp3info"
+require "fileutils"
 
-data_from_reddit = open("http://www.reddit.com/r/listentothis/top.json?sort=top&t=year&limit=10")
+data_from_reddit = open("http://www.reddit.com/r/listentothis/top.json?sort=top&t=month&limit=3")
 #gets top 20 posts from r/listentothis in the past month (usually) in tempfile format
 
 month = Date::MONTHNAMES[Date.today.month]
 #the name of the current month
+
+
 
 
 def make_object(data)
@@ -24,6 +28,7 @@ def make_object(data)
     return ready_version
   end
 end
+
 
 def find_urls(data_object)
   #this method iterates through the data object for each post it is given
@@ -56,26 +61,53 @@ end
 
 
 def add_to_itunes(month)
-  `mv #{month}/ ~/Music/iTunes/iTunes\ Media/Automatically\ Add\ to\ iTunes.localized/`
+  p month
+  p month.class
+  dest = "~/Music/iTunes/iTunes\ Media/Automatically\ Add\ to\ iTunes.localized/"
+  `mv ./April/ /Users/bradleyneal/Music/iTunes/iTunes\ Media/Automatically\ Add\ to\ iTunes.localized`
+  # mv ./April/ ~/Music/iTunes/iTunes\ Media/Automatically\ Add\ to\ iTunes.localized/
+end
+
+def change_to_mp3(month)
+  Dir.glob("./#{month}/*.m4a").each do |f|
+    FileUtils.mv f, "#{File.dirname(f)}/#{File.basename(f,'.*')}.mp3"
+  end
+end
+
+
+def change_back_to_m4a(month)
+  Dir.glob("./#{month}/*.mp3").each do |f|
+    FileUtils.mv f, "#{File.dirname(f)}/#{File.basename(f,'.*')}.m4a"
+  end
 end
 
 def add_metadata(month)
-  files = Dir.entries("./#{month}/")
-  files.each do |file|
-    p file
+  change_to_mp3(month)
+  # dir = "/Users/bradleyneal/Desktop/projects/OpenReddit/#{month}/"
+  dir = "./#{month}/"
+  Dir.entries(dir).each do |file|
+   next if file !~ /.mp3$/ # skip files not ending with .mp3
+   p file
+   Mp3Info.open(dir + file) do |mp3|
+      mp3.tag.album = "#{month}"
+      p mp3.tag.album
+   end
   end
+  change_back_to_m4a(month)
 end
 
 
 def execute_everything(month, data_from_reddit)
-  make_dir(month) #create dir for month
-  objectified_data = make_object(data_from_reddit) #make reddit data an object
-  url_array = find_urls(objectified_data) #scrapes all urls from object, returns array of urls
-  url_array.each_with_index do |url, idx| #iterates over url array and downloads file for each url, adding it to month directory
-    p url, idx
-    run_download(url, month)
-  end
-  p "no more urls"
+  # make_dir(month) #create dir for month
+  # objectified_data = make_object(data_from_reddit) #make reddit data an object
+  # url_array = find_urls(objectified_data) #scrapes all urls from object, returns array of urls
+  # url_array.each_with_index do |url, idx| #iterates over url array and downloads file for each url, adding it to month directory
+  #   p url, idx
+  #   run_download(url, month)
+  # end
+  # p "no more urls"
+  # change_to_mp3(month)
+  # add_metadata(month)
   add_to_itunes(month)
   #still need
     # move to itunes
