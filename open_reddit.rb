57942fdac0581fb89e1d6a7b7656efd224241d2e
Bradneal14 <bradleyhneal@gmail.com>
2016-04-12 18:12:47 -0700

    adds methods for successful metadata tagging

--- open_reddit.rb
+++ open_reddit.rb
@@ -5,13 +5,25 @@ require "date"
 require "mp3info"
 require "fileutils"
 
-data_from_reddit = open("http://www.reddit.com/r/listentothis/top.json?sort=top&t=month&limit=3")
+data_from_reddit = open("http://www.reddit.com/r/listentothis/top.json?sort=top&t=month&limit=5")
 #gets top 20 posts from r/listentothis in the past month (usually) in tempfile format
 
 month = Date::MONTHNAMES[Date.today.month]
 #the name of the current month
 
-
+def tag(month)
+  dir = "./#{month}/"
+  Find.find(dir) do |song|
+    next if song !~ /.m4a$/
+    TagLib::MP4::File.open(song) do |el|
+      tag = el.tag
+      tag.album = "#{month} + MOFUCKA"
+      el.save
+    end
+    # Load an ID3v2 tag from a file
+    # File is automatically closed at block end
+  end
+end  # File is automatically closed at block end
 
 
 def make_object(data)
@@ -84,7 +96,7 @@ def add_metadata(month)
    next if file !~ /.mp3$/ # skip files not ending with .mp3
    p file
    Mp3Info.open(dir + file) do |mp3|
-      mp3.tag.album = "#{month}"
+      mp3.tag.album = "#{month} JACKPACK"
       p mp3.tag.album
    end
   end
@@ -93,15 +105,14 @@ end
 
 
 def execute_everything(month, data_from_reddit)
-  # make_dir(month) #create dir for month
-  # objectified_data = make_object(data_from_reddit) #make reddit data an object
-  # url_array = find_urls(objectified_data) #scrapes all urls from object, returns array of urls
-  # url_array.each_with_index do |url, idx| #iterates over url array and downloads file for each url, adding it to month directory
-  #   p url, idx
-  #   run_download(url, month)
-  # end
-  # p "no more urls"
-  # change_to_mp3(month)
+  make_dir(month) #create dir for month
+  objectified_data = make_object(data_from_reddit) #make reddit data an object
+  url_array = find_urls(objectified_data) #scrapes all urls from object, returns array of urls
+  url_array.each_with_index do |url, idx| #iterates over url array and downloads file for each url, adding it to month directory
+    p url, idx
+    run_download(url, month)
+  end
+  p "no more urls"
   # add_metadata(month)
   add_to_itunes(month)
   #still need
